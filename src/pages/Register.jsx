6a20d6679b28f0a6bbeaa3784c4d4a224f9c16c3
Mahir <mkojak75@gmail.com>
2024-11-02 22:24:16 +0100

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,16 +5,13 @@ import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-import TextField from "@mui/material/TextField";
 import useApiRequest from "../services/useApiRequest";
 import { Formik } from "formik";
 import RegisterForm, { registerSchema } from "../components/RegisterForm";
 
-
 const Register = () => {
-  const {register} = useApiRequest
+  const { register } = useApiRequest;
 
   return (
     <Container maxWidth="sm">
@@ -53,67 +50,24 @@ const Register = () => {
             Register
           </Typography>
 
-          <Formik initialValues={{
-            username: "",
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: ""
-          }}
-          validationSchema={registerSchema}
-          onSubmit={(values, actions)=>{
-            register(values)
-            actions.resetForm()
-            actions.setSubmitting()
-          }}
-          component={(props)=> <RegisterForm {...props}/>}
-          >
+          <Formik
+            initialValues={{
+              username: "",
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={registerSchema}
+            onSubmit={(values, actions) => {
+              register(values);
+              actions.resetForm();
+              actions.setSubmitting();
+            }}
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
 
-          </Formik>
-
-          <Box
-            component="form"
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-          >
-            <TextField
-              label="User Name"
-              name="username"
-              id="userName"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="First Name"
-              name="first_name"
-              id="firstName"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="Last Name"
-              name="last_name"
-              id="last_name"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="Email"
-              name="email"
-              id="email"
-              type="email"
-              variant="outlined"
-            />
-            <TextField
-              label="password"
-              name="password"
-              id="password"
-              type="password"
-              variant="outlined"
-            />
-            <Button type="submit" variant="contained" size="large">
-              Submit
-            </Button>
-          </Box>
+          
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/">Do you have an account?</Link>
--- src/services/useApiRequest.js
+++ src/services/useApiRequest.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
-import { useDispatch } from "react-redux";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  registerSuccess,
+  logoutSuccess,
+} from "../features/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
 
 //? Custom Hook
 const useApiRequest = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth)
   const login = async (userData) => {
     // const BASE_URL = "https://19229.fullstack.clarusway.com";
 
@@ -26,8 +34,30 @@ const useApiRequest = () => {
       console.log(error);
     }
   };
-  const register = async () => {};
-  const logout = async () => {};
+  const register = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/users/`,
+        userInfo
+      );
+      dispatch(registerSuccess(data));
+      navigate("/stock");
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      dispatch(logoutSuccess())
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
   return { login, register, logout };
 };
 
