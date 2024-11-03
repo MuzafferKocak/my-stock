f02d5537adee7d2f2fd3eff30dc8f66fdda49138
Mahir <mkojak75@gmail.com>
2024-11-03 20:35:49 +0100

    useAxios

--- src/services/useApiRequest.js
+++ src/services/useApiRequest.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import {
   fetchFail,
@@ -7,22 +6,25 @@ import {
   registerSuccess,
   logoutSuccess,
 } from "../features/authSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import useAxios from "./useAxios";
 
 //? Custom Hook
 const useApiRequest = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { token } = useSelector((state) => state.auth);
+  const { axiosToken, axiosPublic } = useAxios();
+  // const { token } = useSelector((state) => state.auth);
 
   const login = async (userData) => {
     dispatch(fetchStart());
     try {
-      const data = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/auth/login`,
-        userData
-      );
+      // const data = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/auth/login`,
+      //   userData
+      // );
+      const data = await axiosPublic.post("/auth/login/", userData);
       dispatch(loginSuccess(data));
       toastSuccessNotify("");
       navigate("/stock");
@@ -35,10 +37,11 @@ const useApiRequest = () => {
   const register = async (userInfo) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/users/`,
-        userInfo
-      );
+      // const { data } = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/users/`,
+      //   userInfo
+      // );
+      const { data } = await axiosPublic.post("/users/", userInfo);
       dispatch(registerSuccess(data));
       navigate("/stock");
     } catch (error) {
@@ -48,9 +51,10 @@ const useApiRequest = () => {
   const logout = async () => {
     dispatch(fetchStart());
     try {
-      await axios(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
-        headers: { Authorization: `Token ${token}` },
-      });
+      // await axios(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
+      //   headers: { Authorization: `Token ${token}` },
+      // });
+      await axiosToken.get("/auth/logout");
       dispatch(logoutSuccess());
     } catch (error) {
       dispatch(fetchFail());
