182eb6c5ac1feeddbdcfe0553d9032b2dd43407f
Mahir <mkojak75@gmail.com>
2024-11-04 23:42:38 +0100

    stockSlice

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react"
-import useStockRequest from "../services/useStockRequest"
-
+import { useEffect } from "react";
+import useStockRequest from "../services/useStockRequest";
+import { useSelector } from "react-redux";
 
 const Firms = () => {
+  const { getFirms,getSales } = useStockRequest();
 
-  const {getFirms}= useStockRequest()
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
-    getFirms()
-  }, [])
-  
+    getFirms();
+    getSales()
+  }, []);
+  console.log(firms);
 
-  return (
-    <div>Firms</div>
-  )
-}
+  return <div>Firms</div>;
+};
 
-export default Firms
+export default Firms;
--- src/services/useStockRequest.js
+++ src/services/useStockRequest.js
@@ -1,19 +1,35 @@
 
+import { fetchFail, fetchStart, getFirmsSuccess, getSalesSuccess } from "../features/stockSlice";
 import useAxios from "./useAxios";
-
+import { useDispatch } from "react-redux";
 const useStockRequest = () => {
   const { axiosToken } = useAxios();
+  const dispatch =useDispatch()
   
 
   const getFirms = async () => {
+    dispatch(fetchStart())
     try {
       const {data} = await axiosToken("/firms");
-      console.log(data);
+      
+      dispatch(getFirmsSuccess(data.data))
     } catch (error) {
+      dispatch(fetchFail())
       console.log(error);
     }
   };
-  return { getFirms };
+  const getSales = async () => {
+    dispatch(fetchStart())
+    try {
+      const {data} = await axiosToken("/sales");
+      
+      dispatch(getSalesSuccess(data.data))
+    } catch (error) {
+      dispatch(fetchFail())
+      console.log(error);
+    }
+  };
+  return { getFirms,getSales };
 };
 
 export default useStockRequest;
