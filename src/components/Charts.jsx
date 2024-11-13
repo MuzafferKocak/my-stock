d7d663b3a73f723d249a77c947b0e89fcfa3a360
Mahir <mkojak75@gmail.com>
2024-11-13 17:03:05 +0100

    update Charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,127 +1,119 @@
-
-
 import * as React from "react";
 import { useSelector } from "react-redux";
-import { Grid2 } from "@mui/material"
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart } from "recharts";
+import Box from "@mui/material/Box";
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { Typography } from "@mui/material";
 
 const dataFormatter = (number) => `$${Intl.NumberFormat("us").format(number)}`;
 
 const Charts = () => {
   const { sales, purchases } = useSelector((state) => state.stock);
-  console.log(sales);
-  console.log(purchases);
 
-  
   const salesData =
     sales?.map((item) => ({
-      x: new Date(item.createdAt).toLocaleDateString("de-DE"),
-      y: item.amount,
+      Date: new Date(item.createdAt).toLocaleDateString("de-DE"),
+      Amount: item.amount,
     })) || [];
-  console.log(salesData);
 
   const purchasesData =
     purchases?.map((item) => ({
-      x: new Date(item.createdAt).toLocaleDateString("de-DE"),
-      y: item.amount,
+      Date: new Date(item.createdAt).toLocaleDateString("de-DE"),
+      Amount: item.amount,
     })) || [];
-console.log(purchasesData);
-    
 
   return (
-    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-      {/* Sales için grafik */}
-      <ResponsiveContainer width="48%" height={400}>
-        <LineChart data={salesData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" label={{ value: 'Date', position: 'insideBottomRight' }} />
-          <YAxis label={{ value: 'Amount', angle: -90, position: 'insideLeft' }} />
-          <Tooltip formatter={(value) => dataFormatter(value)} />
-          <Legend />
-          <Line type="monotone" dataKey="y" stroke="#8884d8" />
-        </LineChart>
-      </ResponsiveContainer>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        justifyContent: "space-between",
+        mt: 5,
+        gap: 2,
+      }}
+    >
+      <Box
+        sx={{
+          width: "49%",
+          bgcolor: "#E9EBF8",
+          border: 2,
+          borderRadius: "10px",
+          p: 2,
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="p"
+          sx={{ fontWeight: "bold", color: "#502F4C", p: 1 }}
+        >
+          Total Sales (€)
+        </Typography>
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={salesData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="Date"
+              label={{ value: "", position: "insideBottomRight" }}
+            />
+            <YAxis label={{ value: "", angle: -90, position: "insideLeft" }} />
+            <Tooltip formatter={(value) => dataFormatter(value)} />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="Amount"
+              stroke="#502F4C"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
 
-      {/* Purchases için grafik */}
-      <ResponsiveContainer width="48%" height={400}>
-        <LineChart data={purchasesData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" label={{ value: 'Date', position: 'insideBottomRight' }} />
-          <YAxis label={{ value: 'Amount', angle: -90, position: 'insideLeft' }} />
-          <Tooltip formatter={(value) => dataFormatter(value)} />
-          <Legend />
-          <Line type="monotone" dataKey="y" stroke="#FF0000" />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
-    
-    // <>
-      
-    //   <LineChart
-    //     width={600}
-    //     height={400}
-    //     // data={salesData}
-    //     series={[
-    //       {
-    //         data: salesData,
-    //         dataKey: "y",
-    //         label: "Sales Amount",
-    //         area: true,
-    //         showMark: false,
-    //         color: "indigo",
-    //       },
-    //     ]}
-    //     xAxis={{ dataKey: "x", label: "Date", scaleType: "band" }}
-    //     yAxis={{
-    //       label: "Amount",
-    //       valueFormatter: dataFormatter,
-    //       width: 60,
-    //     }}
-    //     // sx={{
-    //     //   [`& .${lineElementClasses.root}`]: {
-    //     //     fillOpacity: 0.1, 
-    //     //   },
-    //     // }}
-    //   />
-
-      
-    //   {/* <LineChart
-    //     width={600}
-    //     height={400}
-    //     // data={purchasesData}
-    //     series={[
-    //       {
-    //         data: purchasesData,
-    //         dataKey: "y",
-    //         label: "Purchases Amount",
-    //         area: true,
-    //         showMark: false,
-    //         color: "red",
-    //       },
-    //     ]}
-    //     xAxis={[{ dataKey: "x", label: "Date", scaleType: "band" }]}
-    //     yAxis={{
-    //       label: "Amount",
-    //       valueFormatter: dataFormatter,
-    //       width: 60,
-    //     }}
-    //     // sx={{
-    //     //   [`& .${lineElementClasses.root}`]: {
-    //     //     fillOpacity: 0.1, 
-    //     //   },
-    //     // }}
-    //   /> */}
-    // </>
+      <Box
+        sx={{
+          width: "49%",
+          bgcolor: "#E9EBF8",
+          border: 2,
+          borderRadius: "10px",
+          p: 2,
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="p"
+          sx={{ fontWeight: "bold", color: "#7D1538", p: 1 }}
+        >
+          Total Purchases (€)
+        </Typography>
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={purchasesData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="Date"
+              label={{ value: "", position: "insideBottomRight" }}
+            />
+            <YAxis label={{ value: "", angle: -90, position: "insideLeft" }} />
+            <Tooltip formatter={(value) => dataFormatter(value)} />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="Amount"
+              stroke="#7D1538"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
+    </Box>
   );
 };
 
 export default Charts;
-
-
-
-
-
-
-
-
